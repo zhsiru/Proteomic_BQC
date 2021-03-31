167b6ef51248807fbb9cdcde270f95c851c52d81
Sirui <zhsiru@gmail.com>
2021-03-31 13:28:51 -0400

    Update Somalogic_AFR_GWAS1.sh

--- Somalogic_AFR_GWAS1.sh
+++ Somalogic_AFR_GWAS1.sh
@@ -11,4 +11,13 @@
 
 cd /scratch/richards/sirui.zhou/GWAS_proteome/
 protein=($(awk '{ print $1}' Somalogic_list1.txt))
-for ((i=0;i<${#protein[@]};++i)); do gcta64 --mbfile BQC_filelist.txt --fastGWA-lr --grm AFR/Soma_inf_covidpos_AFR --pheno Somalogic_AFR/${protein[i]}_inf_AFR.csv --covar Soma_inf_case_AFR_cov.txt --qcovar Soma_inf_case_AFR_qcov.txt --maf 0.05 --thread-num 2 --out Somalogic_res_AFR/${protein[i]}_inf_AFR_fastGWA_lr; done
+for ((i=0;i<${#protein[@]};++i)); do gcta64 \
+  --mbfile BQC_filelist.txt \
+  --fastGWA-lr \
+  --grm AFR/Soma_inf_covidpos_AFR \
+  --pheno Somalogic_AFR/${protein[i]}_inf_AFR.csv \
+  --covar Soma_inf_case_AFR_cov.txt \
+  --qcovar Soma_inf_case_AFR_qcov.txt \
+  --maf 0.05 \
+  --thread-num 2 \
+  --out Somalogic_res_AFR/${protein[i]}_inf_AFR_fastGWA_lr; done
